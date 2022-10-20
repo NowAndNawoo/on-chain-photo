97d8f2e37c99b8466f90d3c90f84f63ed3e67e57
nawoo <90888775+NowAndNawoo@users.noreply.github.com>
2022-10-20 21:12:58 +0900

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,55 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const accounts = process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    ethereum: {
+      url: process.env.ETHEREUM_URL || '',
+      accounts,
+    },
+    goerli: {
+      url: process.env.GOERLI_URL || '',
+      accounts,
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || '',
+      accounts,
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL || '',
+      accounts,
+    },
+    polygon: {
+      url: process.env.POLYGON_URL || '',
+      accounts,
+    },
+    mumbai: {
+      url: process.env.MUMBAI_URL || '',
+      accounts,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    // apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
+};
+
+export default config;
